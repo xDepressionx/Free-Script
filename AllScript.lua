e725157a668ad47b282e2c68770eae10eb262b45
xDepressionx <87692149+xDepressionx@users.noreply.github.com>
2021-09-29 20:57:16 +0700

    Update AllScript.lua

--- AllScript.lua
+++ AllScript.lua
@@ -1,14 +1,13 @@
 local PlaceID = game.PlaceId
 if PlaceID == 537413528 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/GOJOsatoru1122/SWPRDHUBBUILDABOAT/main/README.md")()
-elseif PlaceID == 2753915549 or PlaceID == 4442272183 or PlaceID == 7449423635 then
+end
+if PlaceID == 2753915549 or PlaceID == 4442272183 or PlaceID == 7449423635 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua")()
-elseif PlaceID == 4520749081 or PlaceID == 6381829480 or PlaceID == 5931540094 then
+end
+if PlaceID == 4520749081 or PlaceID == 6381829480 or PlaceID == 5931540094 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YUMXXXX/HEY-KUY-/main/KUY.lua"))()
-elseif game.PlaceId == 4616652839 or game.PlaceId == 5447073001 tor game.PlaceId == 5084678830 or game.PlaceId == 5431071837 or game.PlaceId == 5431069982 or game.PlaceId == 5943874201 then
+end
+if game.PlaceId == 4616652839 or game.PlaceId == 5447073001 tor game.PlaceId == 5084678830 or game.PlaceId == 5431071837 or game.PlaceId == 5431069982 or game.PlaceId == 5943874201 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YUMXXXX/sinso2/main/README.lua"))()
-else
-    game.Players.LocalPlayer:kick("Script not Support")
-    wait(2)
-    game:Shutdown()
 end
