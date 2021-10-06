21c0ea349a5bf7d92ac787ebb27566b248bfe9f9
xDepressionx <87692149+xDepressionx@users.noreply.github.com>
2021-10-06 18:43:16 +0700

    Update AllScript.lua

--- AllScript.lua
+++ AllScript.lua
@@ -1,13 +1,17 @@
-local PlaceID = game.PlaceId
-if PlaceID == 537413528 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/GOJOsatoru1122/SWPRDHUBBUILDABOAT/main/README.md")()
-end
-if PlaceID == 2753915549 or PlaceID == 4442272183 or PlaceID == 7449423635 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua")()
-end
-if PlaceID == 4520749081 or PlaceID == 6381829480 or PlaceID == 5931540094 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/YUMXXXX/HEY-KUY-/main/KUY.lua"))()
-end
-if game.PlaceId == 4616652839 or game.PlaceId == 5447073001 or game.PlaceId == 5084678830 or game.PlaceId == 5431071837 or game.PlaceId == 5431069982 or game.PlaceId == 5943874201 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/YUMXXXX/sinso2/main/README.lua"))()
+local PlaceId = game.PlaceId
+
+if PlaceId == 6284583030 then
+	loadstring(game:HttpGet"https://raw.githubusercontent.com/YUMXXXX/Pet-Simulator-X-/main/PetX.lua")()
+elseif PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/YUMXXXX/HEY-KUY-/main/KUY.lua"))()
+elseif PlaceId == 4616652839 or PlaceId == 5447073001 or PlaceId == 5084678830 or PlaceId == 5431071837 or PlaceId == 5431069982 or PlaceId == 5943874201 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/YUMXXXX/sinso2/main/README.lua"))()
+elseif PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+	loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua")()
+elseif PlaceId == 537413528 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/GOJOsatoru1122/SWPRDHUBBUILDABOAT/main/README.md"))()
+else
+	game.Players.LocalPlayer:Kick("สคริปไม่มีเกมนี้ ไอ้เวร")
+	wait(1)
+	game:Shutdown()
 end
