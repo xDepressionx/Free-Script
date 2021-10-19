00516c003243e0d7dfe6ff1d6b773c71cd7e6142
xDepressionx <87692149+xDepressionx@users.noreply.github.com>
2021-10-19 20:05:47 +0700

    Update AllScript.lua

--- AllScript.lua
+++ AllScript.lua
@@ -1,8 +1,6 @@
 local PlaceId = game.PlaceId
 
-if PlacId == 1458767429 then
-	loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/Anime-Battle-Arena.lua")()
-elseif PlaceId == 6284583030 then
+if PlaceId == 6284583030 then
 	loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/Pet-Simulator-X.lua")()
 elseif PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/xDepressionx/Free-Script/main/KingLegacy.lua"))()
@@ -12,6 +10,8 @@ elseif PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 t
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua"))()
 elseif PlaceId == 537413528 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/GOJOsatoru1122/SWPRDHUBBUILDABOAT/main/README.md"))()
+elseif PlacId == 1458767429 then
+	loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/Anime-Battle-Arena.lua")()
 else
 	game.Players.LocalPlayer:kick("สคริปไม่มีเกมนี้ ไอ้เวร")
 	wait(1)
