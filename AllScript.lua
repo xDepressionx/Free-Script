b096e9ef44d433cb30be2ef84888ce8e63eae883
xDepressionx <87692149+xDepressionx@users.noreply.github.com>
2022-01-26 21:30:08 +0700

    Update AllScript.lua

--- AllScript.lua
+++ AllScript.lua
@@ -5,7 +5,7 @@ if PlaceId == 6284583030 then
 elseif PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/xDepressionx/Free-Script/main/KingLegacy.lua"))()
 elseif PlaceId == 4616652839 or PlaceId == 5447073001 or PlaceId == 5084678830 or PlaceId == 5431071837 or PlaceId == 5431069982 or PlaceId == 5943874201 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/YUMXXXX/sinso2/main/README.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua"))()
 elseif PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/YUMXXXX/hee.lua/main/README.lua"))()
 elseif PlaceId == 537413528 then
